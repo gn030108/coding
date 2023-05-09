986c635afb3c38b21e0aa2d44ab89e03bec9eb98
Wonwoo Park <gn030108@gmail.com>
2023-05-09 16:33:37 +0900

    master

---
+++ 숫자 맞추기 게임/main.js
@@ -0,0 +1,80 @@
+//랜덤번호 지정 
+//유저가 번호를 입력, go 버튼을 누름
+//만약 유저가 맞추면 , 맞췄습니다
+//작다면 down
+//크다면 up
+//reset 버튼을 누르면 게임이 리셋된다
+//5번의 기회를 다쓰면 게임이 끝난다 {더이상 추측 불가 버튼이 disable}
+//유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깎지 않는다 
+
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput=document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+let resetButton=document.getElementById("reset-button");
+let chances = 5;
+let gameOver =false;
+let chanceArea = document.getElementById("chance-area");
+let history=[];
+
+playButton.addEventListener("click",play);
+resetButton.addEventListener("click",reset);
+userInput.addEventListener("focus", function() {userInput.value=""});
+
+
+
+function pickRandomNum(){
+    computerNum = Math.floor(Math.random()*100)+1;
+    console.log("정답",computerNum);
+}
+
+function play(){
+    let userValue=userInput.value;
+
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="1과100사이 숫자를 입력해 주세요";
+        return;
+    }
+
+    if(history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자 입니다. 다른 숫자를 입력해 주세요";
+        return;
+    } 
+    chances -- ;
+    console.log(chances)
+    chanceArea.textContent=`남은기회 : ${chances}번`; 
+  
+
+    if (userValue < computerNum){
+        resultArea.textContent = "Up!!!!";
+    }
+    else if (userValue > computerNum){
+        resultArea.textContent = "Down!!!";
+    }
+    else {
+        resultArea.textContent = "맞췄습니다!!";
+        gameOver=true;
+    }
+
+    history.push(userValue);
+    console.log(history)
+
+    if(chances ==0){
+        gameOver = true;
+    }
+    
+    if(gameOver == true){
+        playButton.disabled = true;
+    }
+}
+
+function reset(){
+    //user input 창이 깨끗하게 정리되고 
+    userInput.value=""
+    //새로운 번호가 생성되고
+    pickRandomNum();
+    resultArea.textContent = "결과값이 여기 나옵니다."
+}
+
+
+pickRandomNum();
